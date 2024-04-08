0318eadd112a9bc2029f296196241b7c17bc58b3
alexdelargy <addelargy97@gmail.com>
2024-04-08 18:01:07 -0400

    Init

---
+++ FinalCreate.sql
@@ -0,0 +1,77 @@
+
+CREATE TABLE LAB8Movies   (
+    MovieID   int PRIMARY KEY,
+    MovieName   varchar(255),
+    Director   varchar(255),
+    "Description"   varchar(255),
+    AverageRating   int,
+    Runtime   int,
+    ReleaseDate   date,
+    Genre   varchar(255)
+);
+
+CREATE TABLE LAB8UserWatchHistory   (
+    MovieID   int,
+    UserID   int,
+    UserRating   int,
+    PercentageWatched   decimal,
+    WatchDate   date,
+  PRIMARY KEY (  MovieID  ,   UserID  )
+);
+
+CREATE TABLE   LAB8MovieActor   (
+    ActorID   int,
+    MovieID   int,
+    "Role"   varchar(255),
+  PRIMARY KEY (  ActorID  ,   MovieID  )
+);
+
+CREATE TABLE   LAB8Actors   (
+    ActorID   int PRIMARY KEY,
+    Bio   varchar(255),
+    Fname   varchar(255),
+    Lname   varchar(255),
+    Height   int,
+    DOB   date
+);
+
+CREATE TABLE LAB8Users   (
+    UserID   int PRIMARY KEY,
+    Fname   varchar(255),
+    Lname   varchar(255),
+    Email   varchar(255),
+    DOB   date,
+    HomeIP   int,
+    SubscriptionID   int,
+    AddressID   int 
+);
+
+CREATE TABLE   LAB8UserAddress   (
+    AddressID   int PRIMARY KEY,
+    AddressLine1   varchar(255),
+    AddressLine2   varchar(255),
+    City   varchar(255),
+    "State"   varchar(255),
+    ZipCode   int
+);
+
+CREATE TABLE   LAB8Subscription   (
+    SubscriptionID   int PRIMARY KEY,
+    SubscriptionType   varchar(255),
+    RenewalDate   date,
+    Balance   int
+);
+
+ALTER TABLE   LAB8Users   ADD FOREIGN KEY (  SubscriptionID  ) REFERENCES   LAB8Subscription   (  SubscriptionID  );
+
+ALTER TABLE   LAB8UserWatchHistory   ADD FOREIGN KEY (  MovieID  ) REFERENCES   LAB8Movies   (  MovieID  );
+
+ALTER TABLE   LAB8UserWatchHistory   ADD FOREIGN KEY (  UserID  ) REFERENCES   LAB8Users   (  UserID  );
+
+ALTER TABLE   LAB8MovieActor   ADD FOREIGN KEY (  MovieID  ) REFERENCES   LAB8Movies   (  MovieID  );
+
+ALTER TABLE   LAB8MovieActor   ADD FOREIGN KEY (  ActorID  ) REFERENCES   LAB8Actors  (  ActorID  );
+
+ALTER TABLE   LAB8Users ADD FOREIGN KEY (  AddressID  ) REFERENCES   LAB8UserAddress   (  AddressID  );
+
+
