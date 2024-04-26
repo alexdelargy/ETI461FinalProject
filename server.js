2052e69317392652021e179b6a3a154f797355b8
alexdelargy <59781025+alexdelargy@users.noreply.github.com>
2024-04-26 15:31:20 -0400

    Remove password

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const oracledb = require('oracledb');
 const app = express();
 const PORT = 3000;
 
-const dbConfig = {user: "ADMIN", password: "cevbet-fejxAd-fajvi6", connectionString: "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.us-ashburn-1.oraclecloud.com))(connect_data=(service_name=g3072397aa48eb9_yt1zdf20cec0f0k6_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))" };
+const dbConfig = {user: "ADMIN", password: "password", connectionString: "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.us-ashburn-1.oraclecloud.com))(connect_data=(service_name=g3072397aa48eb9_yt1zdf20cec0f0k6_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))" };
 
 app.use(express.json());
 app.use(express.static("express"));
